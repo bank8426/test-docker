a33566b94f5c132f87e671a237905abbdbf1e9ec
Tanapon Kuljarusin <bank_55555@hotmail.com>
2025-05-08 16:09:06 +0700

    use env variable for database credential instead, add .env.example, update docker compose to use .env

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 const express = require("express");
+const mysql = require("mysql2/promise");
 const app = express();
 const port = 8000;
+
+let conn = null;
+
+const initMySQL = async () => {
+  conn = await mysql.createConnection({
+    host: process.env.PMA_HOST,
+    user: process.env.PMA_USER,
+    password: process.env.PMA_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+  });
+};
+
+app.get("/users", async (req, res) => {
+  const [results] = await conn.query("SELECT * FROM users");
+  res.json(results);
+});
 
 app.get("/hello-world", (req, res) => {
   res.send("hello world");
 });
 
-app.listen(port, () => {
+app.listen(port, async () => {
+  await initMySQL();
   console.log(`Server running at http://localhost:${port}/`);
 });
