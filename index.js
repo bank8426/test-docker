b1e8befc0cc19ea89b997cf1b6261484d5f27c3d
Tanapon Kuljarusin <bank_55555@hotmail.com>
2025-05-08 16:30:19 +0700

    set port for db and backend from .env instead

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mysql = require("mysql2/promise");
 const app = express();
-const port = 8000;
+const port = process.env.BACKEND_PORT;
 
 let conn = null;
 
